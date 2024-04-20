326ec580f599da6624c4d9d164a41f528c8c3a8c
Josh Crawford <joshua@sgroup.com.au>
2024-04-20 15:12:10 +1000

    Update non-English translations.

--- src/translations/es/patrol.php
+++ src/translations/es/patrol.php
@@ -1,12 +1,9 @@
 <?php
 
 return [
-    'Patrol' => 'Patrol',
-    'Maintenance Mode' => 'Modo De Mantenimiento',
-    'Maintenance Mode Page URL' => 'Pagina De Mantenimiento',
-    'Authorized IPs' => 'IPs Authorizadas',
-    'SSL Routing' => 'Enrutamiento De SSL',
-    'Restricted URL Segments' => 'Segmentos Restringidos',
-    'Access the site when maintenance mode is enabled' => 'Acceder al sitio cuando el modo de mantenimiento está activado',
-    '{url} is not a valid URL' => '{url} no es una URL valida',
+  'Access the site when maintenance mode is enabled' => 'Acceder al sitio cuando el modo de mantenimiento está activado',
+  'Maintenance Mode' => 'Modo De Mantenimiento',
+  'Patrol' => 'Patrol',
+  'SSL Routing' => 'Enrutamiento De SSL',
+  '{url} is not a valid URL' => '{url} no es una URL valida',
 ];
