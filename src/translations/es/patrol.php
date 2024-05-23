1759a4ee6994a15feb395004995bcb14d4d55bca
Josh Crawford <joshua@sgroup.com.au>
2024-05-23 14:32:35 +1000

    Update non-English translations.

--- src/translations/es/patrol.php
+++ src/translations/es/patrol.php
@@ -2,8 +2,17 @@
 
 return [
   'Access the site when maintenance mode is enabled' => 'Acceder al sitio cuando el modo de mantenimiento está activado',
+  'Authorized IPs' => 'IPs Authorizadas',
+  'Enable Maintenance Mode' => 'Enable Maintenance Mode',
   'Maintenance Mode' => 'Modo De Mantenimiento',
+  'Maintenance Mode Page URL' => 'Pagina De Mantenimiento',
   'Patrol' => 'Patrol',
+  'Restricted URL Segments' => 'Segmentos Restringidos',
   'SSL Routing' => 'Enrutamiento De SSL',
+  'The IP addresses that should be able to access the site on maintenance mode. Enter an IP per-line.' => 'The IP addresses that should be able to access the site on maintenance mode. Enter an IP per-line.',
+  'The URL segments where HTTPS should be enforced. Enter a URL per-line.' => 'The URL segments where HTTPS should be enforced. Enter a URL per-line.',
+  'The URL to redirect unauthorized traffic to if maintenance mode is enabled.' => 'The URL to redirect unauthorized traffic to if maintenance mode is enabled.',
+  'Whether maintenance mode should be enabled.' => 'Whether maintenance mode should be enabled.',
+  'Whether SSL routing and force HTTPS should be enabled.' => 'Whether SSL routing and force HTTPS should be enabled.',
   '{url} is not a valid URL' => '{url} no es una URL valida',
 ];
